e186d990cc23111d94000c0c0a8cd4812c99724d
oximeeg <oximeeg@gmail.com>
2025-07-26 20:03:57 +0900

    3.32.8

--- update.zsh
+++ update.zsh
@@ -11,3 +11,4 @@ flutter_version=`fvm releases | grep stable | tail -n 1`
 flutter_version=`echo $flutter_version | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g" | awk '{print $4}'`
 sed -i '' -E "s/\"flutter\": \"[^\"]+\"/\"flutter\": \"$flutter_version\"/" .fvmrc
 __println "$flutter_version"
+fvm global $flutter_version
